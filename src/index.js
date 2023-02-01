cbb4f37e5d9bb0c00878265208e18a9717ac91b9
stroich <stroichmwork@gmail.com>
2023-02-01 19:13:03 +0400

    feat: add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = bracketsConfig.map(element=>{
+        return element[0]+element[1];
+    })
+    let stack = [];
+    for (let i=0; i<str.length; i++){
+        if (bracketsConfig.map(element=>element[0]).includes(str[i])){
+            if(stack[stack.length-1]===str[i] && ['7','8','|'].includes(str[i])){
+                stack.pop()
+            }else {
+                stack.push(str[i]);
+            }
+        }else {
+            if (stack.length===0){
+                return false;
+            }
+            let config = `${stack[stack.length-1]}${str[i]}`;
+            if(brackets.includes(config)){
+                stack.pop()
+            }else{
+                return false
+            }
+
+        }
+    }
+    if (stack.length===0){
+        return true;
+    }else {
+        return false
+    }
 }
